5d7b57143dc3fdb5577ad6acb428b976b41f9473
Ritam10-coder <ritamkarmakar22@gmail.com>
2024-08-09 22:21:31 +0530

    Update 1.c

--- 1st Semester/1.c
+++ 1st Semester/1.c
@@ -1,1 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char s[]="arijit";
+	char t;
+	int l;
+	l=strlen(s);
+	t=s[0];
+	s[0]=s[l-1];
+	s[l-1]=t;
+	printf("original string : %s",s);
+	return 0;
+}
 
